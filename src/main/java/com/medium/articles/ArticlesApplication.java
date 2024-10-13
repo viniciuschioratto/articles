8f303894b5cb6b58a88a98ddc536cea197843e1e
Vinicius Chioratto <viniciusmchioratto@gmail.com>
2024-10-13 15:08:32 +0100

    feat: init articles project

---
+++ src/main/java/com/medium/articles/ArticlesApplication.java
@@ -0,0 +1,13 @@
+package com.medium.articles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticlesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticlesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medium/articles/ArticlesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medium.articles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticlesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
